ca682c6977e4b79c4feac134273f0d7e069ae691
oal <oweis.al-agtash@objective-partner.com>
2022-01-18 13:19:55 +0100

    editing the font and changing the color of the background

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -168,10 +168,11 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 200,
+      typeSpeed: 100,
       backSpeed: 50,
-      backDelay: 500
+      backDelay: 200
     });
+    
   }
 
   /**
