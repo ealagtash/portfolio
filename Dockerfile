70e51e79f63219d5dc3f233d66e5e167121a31b6
Oweis Al-Agtash <oweis.al-agtash@objective-partner.com>
2022-05-29 00:16:57 +0200

    Half working
    Signed-off-by: Oweis Al-Agtash <oweis.al-agtash@objective-partner.com>

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,10 @@
-FROM python:3.9.10-alpine
+FROM python:3.9.10-slim-buster
 
-ENV MICRO_SERVICE=/home/app/microservice
-RUN addgroup -S ehsan && adduser -S ehsan -G ehsan
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 
+WORKDIR /home/app
 
-RUN mkdir -p $MICRO_SERVICE
-RUN mkdir -p $MICRO_SERVICE/static
+COPY requirements.txt .
 
-# where the code lives
-WORKDIR $MICRO_SERVICE
-
-# set environment variables
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
-
-# install psycopg2 dependencies
-RUN apk update \
-    && apk add --virtual build-deps gcc python3-dev musl-dev \
-    && apk add postgresql-dev gcc python3-dev musl-dev \
-    && apk del build-deps \
-    && apk --no-cache add musl-dev linux-headers g++
-# install dependencies
-RUN pip install --upgrade pip
-# copy project
-COPY . $MICRO_SERVICE
 RUN pip install -r requirements.txt
-COPY ./entrypoint.sh $MICRO_SERVICE
-
-CMD ["/bin/bash", "/home/app/microservice/entrypoint.sh"]
--- Ehsan/settings.py
+++ Ehsan/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-=jw=eah$(is0unhmq6iy-qpq3$pe9xaic1hcg+#v4o7b&7n)^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ealagtash.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -111,12 +112,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'generated-statics')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
