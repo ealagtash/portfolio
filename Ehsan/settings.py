380712a9f5f3002191d8d34998156531bea68921
ealagtash <97917310+ealagtash@users.noreply.github.com>
2022-08-26 23:48:00 -0700

    half working

--- Ehsan/settings.py
+++ Ehsan/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))=="1"
 
-ALLOWED_HOSTS = []
-if not DEBUG:
-    ALLOWED_HOSTS +=[os.environ.get('DJANGO_ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,6 +53,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Ehsan.urls'
+LOGIN_URL='/login/'
 
 TEMPLATES = [
     {
