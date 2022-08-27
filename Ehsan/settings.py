3cc5a05bc90bc11e4381249979d5c65456cd7eda
ealagtash <97917310+ealagtash@users.noreply.github.com>
2022-08-26 21:23:48 -0700

    upgrading setting code and adding dotenv file an updating security

--- Ehsan/settings.py
+++ Ehsan/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import sys
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=jw=eah$(is0unhmq6iy-qpq3$pe9xaic1hcg+#v4o7b&7n)^9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG'))=="1"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS +=[os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -80,6 +84,31 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+POSTGRES_DB = os.environ.get("POSTGRES_DB")#database name
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")#password
+POSTGRES_USER = os.environ.get("POSTGRES_USER")#username
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")#host
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")#port
+
+POSTGRES_READY = (
+    POSTGRES_DB is not None
+    and POSTGRES_PASSWORD is not None
+    and POSTGRES_USER is not None
+    and POSTGRES_HOST is not None
+    and POSTGRES_PORT is not None
+)
+
+if POSTGRES_READY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_DB,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": POSTGRES_HOST,
+            "PORT": POSTGRES_PORT,
+        }
+    }
 
 
 # Password validation
