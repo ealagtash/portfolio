e294c5faa0e0de1d8cd75902cbdd30cb9a0928ca
ealagtash <97917310+ealagtash@users.noreply.github.com>
2022-08-26 21:45:29 -0700

    cleaning

--- Portfolio 2.0/Agtash/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import projects
-from .models import resume 
-
-admin.site.register(projects)
-#admin.site.register(resume)
-# Register your models here.
--- Portfolio 2.0/Agtash/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-#from django.urls import re_path
-
-
-urlpatterns = [
-    path('',views.index, name='index'),
-    path('profile', views.profile ),
-]
